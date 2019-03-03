0f51dab5e8c805e1df61db21c45e734a3d387162
solidbullet <691622093@qq.com>
2019-03-03 20:23:21 +0800

    up1

--- 龙头/dragon.py
+++ 龙头/dragon.py
@@ -10,23 +10,25 @@ def cal_ban_num( stock,begindate,enddate ):
     i = 0
     while i < len(df):
         #print(df['rise'][len(df) - i - 1])
-        i += 1
-        if df['rise'][len(df) - i - 1] < 0.099:
+
+        if df['rise'][len(df) - i - 1] < 0.09:
             break
+        i += 1
     return i
 
 #stock = '601519.XSHG'
-sdate = '2019-02-01'
-edate = '2019-02-27'
+sdate = '2019-02-15'
+edate = '2019-03-02'
 #print(cal_ban_num(stock,sdate,edate))
-dict = {'id':['000000.XXXX'],'count':[0]}
+dict = {'id':['000000.XXXX'],'name':['YYYY'],'count':[0]}
 dict_df = pd.DataFrame(dict)
 stocks = list(get_all_securities(['stock']).index)
-#stocks = ['601519.XSHG','600604.XSHG','600009.XSHG']
+#stocks = ['601865.XSHG','600604.XSHG','600009.XSHG']
 for x in stocks:
     #print(x)
-    dict1 = {'id':x,'count':[cal_ban_num(x,sdate,edate)]}
+    dict1 = {'id':x,'name':[get_security_info(x).display_name],'count':[cal_ban_num(x,sdate,edate)]}
     d = pd.DataFrame(dict1)
     dict_df = dict_df.append(d)
-dragon = dict_df[dict_df['count'] > 1].sort_values(by = 'count',ascending = False).iloc[0:30]
+dragon = dict_df[dict_df['count'] >= 1].sort_values(by = 'count',ascending = False) #.iloc[0:100]
+dragon.to_csv('c:\\Result.csv')
 print(dragon)
